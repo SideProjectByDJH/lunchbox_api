f08b9654a6b299d4fdc40b02a4ee611f18f65f6f
Dong Sub Kim <dskim9218@gmail.com>
2024-03-17 15:19:27 +0900

    initial setup

---
+++ src/main/java/com/djh/lunchbox_api/LunchboxApiApplication.java
@@ -0,0 +1,13 @@
+package com.djh.lunchbox_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LunchboxApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LunchboxApiApplication.class, args);
+    }
+
+}
