522c891c526cf0714592be67ddb7bf42c73a80f8
Dong Sub Kim <dskim9218@gmail.com>
2024-04-10 16:24:19 +0900

    add UserRepository

---
+++ src/main/java/com/djh/lunchbox_api/repository/UserRepository.java
@@ -0,0 +1,7 @@
+package com.djh.lunchbox_api.repository;
+
+import com.djh.lunchbox_api.entity.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, String> {
+}
