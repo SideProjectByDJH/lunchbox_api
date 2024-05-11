bba447bdce080afc5233545ba0f6c42a3ebdab08
Dong Sub Kim <dskim9218@gmail.com>
2024-05-11 15:18:13 +0900

    add Follow

---
+++ src/main/java/com/djh/lunchbox_api/entity/Follow.java
@@ -0,0 +1,21 @@
+package com.djh.lunchbox_api.entity;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+@Entity
+@Data
+public class Follow {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @JoinColumn(name = "follower_id")
+    private User follower;
+
+    @ManyToOne
+    @JoinColumn(name = "followee_id")
+    private User followee;
+}
